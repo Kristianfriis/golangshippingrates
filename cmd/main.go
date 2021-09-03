a7a536713e76a7e06c5b15b972269c34d46b0473
Kristian Friis <friis1212@gmail.com>
2021-09-03 14:49:00 +0200

    got closer to serving static files

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	r := gin.Default()
+	r.Static("/main", "./pkg/site/private/main")
+	r.Use(static.Serve("/site", static.LocalFile("./pkg/site/private", true)))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "ping")
+	})
+
+	// r.Static("/assets", "./assets")
+
+	r.Run()
 }
